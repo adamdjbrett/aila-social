5a46004b32db21a9c50eb3c1e24bc14871ada5a8
mesinkasir <hockeycorpmarketing@gmail.com>
2024-12-12 10:34:10 +0700

    update adam dj style

---
+++ _data/metadata.js
@@ -0,0 +1,19 @@
+export default {
+	title: "American Indian Law Alliance - Social Media",
+	url: "https://aila.social",
+	favicon: "/img/apple-touch-icon.png",
+	language: "en",
+	locale: "en_US",
+	description: "The American Indian Law Alliance (AILA) is an Indigenous NGO that works with Indigenous nations, communities, and organizations in our struggle for sovereignty, human rights, and social justice for our peoples.",
+	metasocial: {
+		image: "/img/potos.jpg",
+		facebook: "https://facebook.com/americanindianlawalliance",
+		twitteruser: "@AILAnyc",
+		twitter: "https://twitter.com/AILAnyc"
+	},
+	author: {
+		name: "Adam Dj Brett",
+		email: "info@adamdjbrett.com",
+		url: "https://adamdjbrett.com"
+	}
+}
